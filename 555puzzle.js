cc66c471d1ea951f3f625b4386b4dfd4c364f108
Noah Porcelli <noahporcelli@gmail.com>
2014-08-11 17:10:21 +0100

    Cleanup
    Changed generateNextConfig to generateConfig and made it much cleaner and without globals
    Cleaned up configWorks
    
    Signed-off-by: Noah Porcelli <noahporcelli@gmail.com>

--- 555puzzle.js
+++ 555puzzle.js
@@ -11,13 +11,9 @@ var arrangement = [
 [35, 55, 55, 70],
 [55, 55, 60, 75]
 ];
-// var arrangement = [
-// [5, 10, 15, 20],
-// [20, 15, 10, 5]
-// ];
+var width = arrangement[0].length;
 var len = arrangement.length;
 var targ = 555;
-// var targ = 25;
 
 //Returns the length of an object
 function length(obj) {
@@ -36,38 +32,44 @@ function print(txt) {
 
 //Returns whether or not a configuration works, given the (global) arrangement of the numbers
 function configWorks(config) {
-	//This function needs to be changed so it takes arrangement as an argument and also has support if puzzle has a different number of sides than 4.
-	var sum1 = 0;
-	var sum2 = 0;
-	var sum3 = 0;
-	var sum4 = 0;
+	//This function needs to be changed so it takes arrangement
+	var sums = {};
+	for (var i = 0; i < width; i++) {
+		sums[i] = 0;
+	}
+
+
 	if (length(config) > len) {
-		// print(config);
 		return 0;
 	}
 	for (var i = 0; i < len; i++) {
-		if (sum1 > targ || sum2 > targ || sum3 > targ || sum4 > targ) {
+		for (var j = 0; j < width; j++) {
+			if (sums[j] > targ) {
+				return 0;
+			}
+		}
+		for (var j = 0; j < width; j++) {
+			sums[j] += arrangement[i][(config[i] + j) % width];
+		}
+	}
+	for (var i = 0; i < width; i++) {
+		if (sums[i] != targ) {
 			return 0;
 		}
-		sum1 += arrangement[i][(config[i] + 1) % 4];
-		sum2 += arrangement[i][(config[i] + 2) % 4];
-		sum3 += arrangement[i][(config[i] + 3) % 4];
-		sum4 += arrangement[i][(config[i] + 4) % 4];
 	}
-	return (sum1 == targ && sum2 == targ && sum3 == targ && sum4 == targ);
+	return 1;
 }
 
 //Iterator that generates next configuration
-var __generateNextConfig_iteratorCount = 0;
-function generateNextConfig() {
+// var __generateNextConfig_iteratorCount = 0;
+function generateNextConfig(iter) {
 	//This funciton needs to be changed so that it takes the iterator as an argument
 	var len = arrangement.length;
 	var numConfigs = Math.pow(4, len - 1);
-	//numConfigs /= 4;
-	if (__generateNextConfig_iteratorCount >= numConfigs) {
+	if (iter >= numConfigs) {
 		return 0;
 	}
-	var ans1 = __generateNextConfig_iteratorCount.toString(4).split("");
+	var ans1 = iter.toString(4).split("");
 	var ans2 = {}
 	for (var m = 0; m < len; m++) {
 		ans2[m] = 0;
@@ -79,22 +81,22 @@ function generateNextConfig() {
 			return 0;
 		}
 	}
-	__generateNextConfig_iteratorCount++;
 	return ans2;
 }
-print(Math.pow(4, len-1));
-print(Math.pow(4, len-1).toString(4));
+
 //Solves the 555 thing
 function doStuff() {
 	//This function needs to be changed?
-	var j = generateNextConfig();
+	var iter = 0;
+	var j = generateNextConfig(iter);
 	var numSolutions = 0;
 	while (j != 0) {
 		if (configWorks(j)) {
 			print(j);
 			numSolutions++;
 		}
-		j = generateNextConfig();
+		iter++;
+		j = generateNextConfig(iter);
 	}
 	print(numSolutions);
 }
